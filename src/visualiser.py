59661011e6cd167e801fb1689aaf4821c8124eda
fourdozen <48531190+fourdozen@users.noreply.github.com>
2024-03-27 13:39:13 +0000

    Plot basic data

--- src/visualiser.py
+++ src/visualiser.py
@@ -6,41 +6,69 @@ import numpy as np
 from datetime import datetime
 
 class Visualiser():
+
     def __init__(self) -> None:
-        pass
+        self.__read_data()
+        self.order_book['Transaction UTC'] = self.__get_datetime(self.order_book)
+        self.public_trade['Transaction UTC'] = self.__get_datetime(self.public_trade)
 
     def visualise(self):
-        self.__read_data()
-        self.__plot_price()
+        self.plot_basic_data()
+        
+    def __read_data(self, path = 'data/sample/'):
+        ob_store = pd.HDFStore(path + 'order_book.h5', mode='r')
+        pt_store = pd.HDFStore(path + 'public_trade.h5', mode='r')
+        self.order_book = ob_store.get('df')
+        self.public_trade = pt_store.get('df')
+        ob_store.close()
+        pt_store.close()
 
-    def __read_data(self, path = 'data/sample/order_book.h5'):
-        store = pd.HDFStore(path, mode='r')
-        self.df = store.get('df')
-        store.close()
-
-    def __get_mid_price(self):
-        self.df['Mid price'] = 0.5 * (self.df['Bid price'] + self.df['Ask price'])
-        return self.df['Mid price']
+    @staticmethod
+    def __get_mid_price(df):
+        df['Mid price'] = 0.5 * (df['Bid price'] + df['Ask price'])
+        return df['Mid price']
     
     @staticmethod
     def __get_datetime(df):
         timestamps = np.array(df["Transaction time"])
         utc = np.array([datetime.fromtimestamp(ts) for ts in timestamps])
         return utc
+    
+    @staticmethod
+    def __get_spread(df):
+        df['Spread'] = df['Ask price'] - df['Bid price']
+        return df['Spread']
 
-    def __plot_price(self):
-        self.df['Transaction UTC'] = self.__get_datetime(self.df)
-        self.__get_mid_price()
+    def plot_mid_price(self, fig, ax):
+        self.__get_mid_price(self.order_book)
         xfmt = mpl.dates.DateFormatter('%Y-%m-%d %H:%M:%S')
-        ax = plt.gca()
         ax.xaxis.set_major_formatter(xfmt)
-        plt.plot(self.df['Transaction UTC'], self.df['Mid price'])
+        ax.plot(self.order_book['Transaction UTC'], self.order_book['Mid price'])
         plt.xticks(rotation=25, ha='right')
         plt.subplots_adjust(left=0.2, bottom=0.3)
-        plt.title('Mid Price')
-        plt.xlabel('UTC Time')
-        plt.ylabel('(USD?)')
-        plt.savefig('price.png')
+        ax.set_title('Order Book Mid Price')
+        ax.set_ylabel('Price (USD)')
+
+    def plot_volume(self, fig, ax):
+        ax.plot(self.public_trade['Transaction UTC'], np.abs(self.public_trade['Trade qty']))
+        ax.set_title('Volume')
+        ax.set_ylabel('Traded Qty (Volume)')
+
+    def plot_spread(self, fig, ax):
+        self.__get_spread(self.order_book)
+        ax.plot(self.order_book['Transaction UTC'], self.order_book['Spread'])
+        ax.set_title('Spread')
+        ax.set_ylabel('(USD)')
+
+    def plot_basic_data(self):
+        fig, (ax0, ax1, ax2) = plt.subplots(3, 1, sharex='col', figsize=(10.5, 13.5))
+        self.plot_mid_price(fig, ax0)
+        self.plot_volume(fig, ax1)
+        self.plot_spread(fig, ax2)
+        plt.xlabel('Time UTC')
+        plt.tight_layout()
+        plt.savefig('Basic_data.png', dpi=300)
+
 
 if __name__ == '__main__':
     Visualiser().visualise()
