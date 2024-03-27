a4e950a1311cbae0e61708248a004eee12100da7
fourdozen <48531190+fourdozen@users.noreply.github.com>
2024-03-27 15:53:13 +0000

    Create event_analyser.py

---
+++ src/event_analyser.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+class EventAnalyser():
+    def __init__(self, order_book: pd.DataFrame, public_trade: pd.DataFrame):
+        self.order_book = order_book
+        self.public_trade = public_trade
+
+    def bin_data():
+        pass
+
+    def get_relative_price_change():
+        pass
