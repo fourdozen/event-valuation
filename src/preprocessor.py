aa3ec5877f982846fbecf2b8656d54e2370a047a
fourdozen <48531190+fourdozen@users.noreply.github.com>
2024-06-28 16:38:19 +0100

    Create preprocessor.py

---
+++ src/preprocessor.py
@@ -0,0 +1,21 @@
+from hdf5reader import HDF5Reader
+
+class Preprocessor():
+
+    def __init__(self, df):
+        self.df = df
+
+    def bucket_data(self, size):
+        pass
+
+    def get_mid_price():
+        pass
+
+
+class OrderBookPreprocessor(Preprocessor):
+
+    def __init__(self, df):
+        super().__init__(df)
+
+    def bucket_data(self, size):
+        pass
